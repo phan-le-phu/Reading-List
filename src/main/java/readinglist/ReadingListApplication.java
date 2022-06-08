26c5fb065df9d91981a6a5e439ee2253c13f0fc2
phanlephu <phuphanle49@gmail.com>
2022-06-08 09:09:03 -0700

    test dev branch

--- src/main/java/readinglist/ReadingListApplication.java
+++ src/main/java/readinglist/ReadingListApplication.java
@@ -12,11 +12,11 @@ public class ReadingListApplication {
 		SpringApplication.run(ReadingListApplication.class, args);
 	}
 
-	@Bean
+/*	@Bean
 	public CommandLineRunner run(ReaderRepository repository) {
 		return (args -> {
 			repository.save(new Reader("phu", "phanlephu", "{noop}123456"));
 		});
-	}
+	}*/
 
 }
